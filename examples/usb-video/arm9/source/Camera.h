d161bc6700d2d5292555f2eb2f0ba93182160169
Atomlabor <46759744+atomlabor@users.noreply.github.com>
2026-02-20 14:02:00 +0100

    Delete examples/usb-video directory

--- examples/usb-video/arm9/source/Camera.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-void cam_init(bool front);
-void cam_stop();
-void cam_switch();
-void cam_dmaStart(int dma, void* dst);
--- examples/usb-video/common/IpcChannels.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-#define IPC_CHANNEL_CAMERA  16
-#define IPC_CHANNEL_CAPTURE 17
