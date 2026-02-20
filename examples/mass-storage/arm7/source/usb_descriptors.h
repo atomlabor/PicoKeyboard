2aca48d892d7d5ccda328326f747cd4c00b6c0e1
Atomlabor <46759744+atomlabor@users.noreply.github.com>
2026-02-20 14:01:00 +0100

    Delete examples/mass-storage directory

--- examples/mass-storage/arm7/source/usb_descriptors.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-enum
-{
-    ITF_NUM_MSC,
-    ITF_NUM_TOTAL
-};
--- examples/mass-storage/common/ipcChannels.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#define IPC_CHANNEL_DSI_SD  16
-#define IPC_CHANNEL_DLDI    17
-#define IPC_CHANNEL_LOADER  18
-#define IPC_CHANNEL_SOUND   19
-#define IPC_CHANNEL_RTC     20
