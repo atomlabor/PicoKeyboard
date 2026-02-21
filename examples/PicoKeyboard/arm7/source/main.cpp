9348daf0034b687d10f54e69ccfbbe9443687abc
Atomlabor <46759744+atomlabor@users.noreply.github.com>
2026-02-21 12:47:37 +0100

    Refactor main function by removing card and IPC setup
    Removed card initialization and IPC service setup from main function.

--- examples/PicoKeyboard/arm7/source/main.cpp
+++ examples/PicoKeyboard/arm7/source/main.cpp
@@ -1,35 +1,19 @@
 #include "common.h"
-#include <libtwl/card/card.h>
 #include <libtwl/rtos/rtosIrq.h>
 #include <libtwl/rtos/rtosThread.h>
 #include "tusb.h"
-#include "DldiIpcService.h"
-#include "Arm7State.h"
-#include "ExitMode.h"
 
 // Global card mutex (declared extern in common.h)
 rtos_mutex_t gCardMutex;
 
-static DldiIpcService sDldiIpcService;
-
 int main()
 {
-    // Initialize the card mutex
     rtos_createMutex(&gCardMutex);
 
-    // Initialize card hardware
-    card_init();
-
-    // Initialize TinyUSB device stack
     tud_init(BOARD_TUD_RHPORT);
 
-    // Start IPC service threads
-    sDldiIpcService.Start();
-
-    // Enable interrupts
     rtos_enableIrqs();
 
-    // Main loop: keep the USB device stack running
     while (true)
     {
         tud_task();
