eef4914ca62731b318d17e427f2efcf07b2b9746
Atomlabor <46759744+atomlabor@users.noreply.github.com>
2026-02-21 13:04:34 +0100

    Implement keyboard FIFO handler in main.cpp
    Added keyboard FIFO handler for processing keyboard reports.

--- examples/PicoKeyboard/arm7/source/main.cpp
+++ examples/PicoKeyboard/arm7/source/main.cpp
@@ -1,14 +1,33 @@
 #include "common.h"
 #include <libtwl/rtos/rtosIrq.h>
-#include <libtwl/rtos/rtosThread.h>
 #include "tusb.h"
 
-// Global card mutex (declared extern in common.h)
 rtos_mutex_t gCardMutex;
+
+// FIFO-Kanal muss identisch mit ARM9 sein
+#define FIFO_KEYBOARD  FIFO_USER_01
+
+// Wird vom ARM9 aufgerufen wenn ein Keyboard-Report ankommt
+static void keyboard_fifo_handler(u32 value, void* userdata)
+{
+    (void)userdata;
+
+    uint8_t modifier = (value >> 24) & 0xFF;
+    uint8_t keycode  = (value >> 16) & 0xFF;
+
+    if (tud_hid_ready())
+    {
+        uint8_t keycodes[6] = { keycode, 0, 0, 0, 0, 0 };
+        tud_hid_keyboard_report(0, modifier, keycodes);
+    }
+}
 
 int main()
 {
     rtos_createMutex(&gCardMutex);
+
+    // FIFO-Handler registrieren bevor IRQs aktiviert werden
+    fifoSetValue32Handler(FIFO_KEYBOARD, keyboard_fifo_handler, NULL);
 
     tud_init(BOARD_TUD_RHPORT);
 
