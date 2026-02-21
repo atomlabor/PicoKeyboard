1fe4d300ee7821956ef8d1d4af976589c57456b7
Atomlabor <46759744+atomlabor@users.noreply.github.com>
2026-02-21 11:52:37 +0100

    Update USB configuration for HID and audio settings

--- examples/PicoKeyboard/arm7/source/tusb_config.h
+++ examples/PicoKeyboard/arm7/source/tusb_config.h
@@ -8,13 +8,10 @@ extern "C" {
 //--------------------------------------------------------------------+
 // Board Specific Configuration
 //--------------------------------------------------------------------+
-
-// RHPort number used for device can be defined by board.mk, default to port 0
 #ifndef BOARD_TUD_RHPORT
 #define BOARD_TUD_RHPORT      0
 #endif
 
-// RHPort max operational speed can defined by board.mk
 #ifndef BOARD_TUD_MAX_SPEED
 #define BOARD_TUD_MAX_SPEED   OPT_MODE_DEFAULT_SPEED
 #endif
@@ -22,8 +19,6 @@ extern "C" {
 //--------------------------------------------------------------------
 // COMMON CONFIGURATION
 //--------------------------------------------------------------------
-
-// defined by compiler flags for flexibility
 #ifndef CFG_TUSB_MCU
 #define CFG_TUSB_MCU          OPT_MCU_DSPICO
 #endif
@@ -38,19 +33,9 @@ extern "C" {
 #define CFG_TUSB_DEBUG        0
 #endif
 
-// Enable Device stack
 #define CFG_TUD_ENABLED       1
-
-// Default is max speed that hardware controller could support with on-chip PHY
 #define CFG_TUD_MAX_SPEED     BOARD_TUD_MAX_SPEED
 
-/* USB DMA on some MCUs can only access a specific SRAM region with restriction on alignment.
- * Tinyusb use follows macros to declare transferring memory so that they can be put
- * into those specific section.
- * e.g
- * - CFG_TUSB_MEM SECTION : __attribute__ (( section(".usb_ram") ))
- * - CFG_TUSB_MEM_ALIGN   : __attribute__ ((aligned(4)))
- */
 #ifndef CFG_TUSB_MEM_SECTION
 #define CFG_TUSB_MEM_SECTION
 #endif
@@ -62,56 +47,23 @@ extern "C" {
 //--------------------------------------------------------------------
 // DEVICE CONFIGURATION
 //--------------------------------------------------------------------
-
 #ifndef CFG_TUD_ENDPOINT0_SIZE
 #define CFG_TUD_ENDPOINT0_SIZE    64
 #endif
 
 //------------- CLASS -------------//
+// Hier aktivieren wir AUSSCHLIESSLICH die Tastatur (HID)
 #define CFG_TUD_CDC               0
 #define CFG_TUD_MSC               0
-#define CFG_TUD_HID               0
+#define CFG_TUD_HID               1
 #define CFG_TUD_MIDI              0
-#define CFG_TUD_AUDIO             1
+#define CFG_TUD_AUDIO             0
 #define CFG_TUD_VENDOR            0
 
 //--------------------------------------------------------------------
-// AUDIO CLASS DRIVER CONFIGURATION
+// HID CLASS DRIVER CONFIGURATION
 //--------------------------------------------------------------------
-
-// Enable if Full-Speed on OSX, also set feedback EP size to 3
-#define CFG_TUD_AUDIO_ENABLE_FEEDBACK_FORMAT_CORRECTION              0
-
-#define CFG_TUD_AUDIO_ENABLE_FEEDBACK_EP                             1
-
-#define CFG_TUD_AUDIO_FUNC_1_DESC_LEN                                TUD_AUDIO_SPEAKER_STEREO_FB_DESC_LEN
-
-// How many formats are used, need to adjust USB descriptor if changed
-#define CFG_TUD_AUDIO_FUNC_1_N_FORMATS                               1
-
-// Audio format type I specifications
-/* 24bit/48kHz is the best quality for headset or 24bit/96kHz for 2ch speaker,
-   high-speed is needed beyond this */
-#define CFG_TUD_AUDIO_FUNC_1_MAX_SAMPLE_RATE                         44100
-#define CFG_TUD_AUDIO_FUNC_1_N_CHANNELS_RX                           2
-
-// 16bit in 16bit slots
-#define CFG_TUD_AUDIO_FUNC_1_FORMAT_1_N_BYTES_PER_SAMPLE_RX          2
-#define CFG_TUD_AUDIO_FUNC_1_FORMAT_1_RESOLUTION_RX                  16
-
-// EP and buffer size - for isochronous EP's, the buffer and EP size are equal (different sizes would not make sense)
-#define CFG_TUD_AUDIO_ENABLE_EP_OUT               1
-
-#define CFG_TUD_AUDIO_FUNC_1_FORMAT_1_EP_SZ_OUT   TUD_AUDIO_EP_SIZE(CFG_TUD_AUDIO_FUNC_1_MAX_SAMPLE_RATE, CFG_TUD_AUDIO_FUNC_1_FORMAT_1_N_BYTES_PER_SAMPLE_RX, CFG_TUD_AUDIO_FUNC_1_N_CHANNELS_RX)
-
-#define CFG_TUD_AUDIO_FUNC_1_EP_OUT_SW_BUF_SZ     (CFG_TUD_AUDIO_FUNC_1_FORMAT_1_EP_SZ_OUT*8)
-#define CFG_TUD_AUDIO_FUNC_1_EP_OUT_SZ_MAX        CFG_TUD_AUDIO_FUNC_1_FORMAT_1_EP_SZ_OUT // Maximum EP IN size for all AS alternate settings used
-
-// Number of Standard AS Interface Descriptors (4.9.1) defined per audio function - this is required to be able to remember the current alternate settings of these interfaces - We restrict us here to have a constant number for all audio functions (which means this has to be the maximum number of AS interfaces an audio function has and a second audio function with less AS interfaces just wastes a few bytes)
-#define CFG_TUD_AUDIO_FUNC_1_N_AS_INT 	          1
-
-// Size of control request buffer
-#define CFG_TUD_AUDIO_FUNC_1_CTRL_BUF_SZ	64
+#define CFG_TUD_HID_EP_BUFSIZE    16
 
 #ifdef __cplusplus
 }
